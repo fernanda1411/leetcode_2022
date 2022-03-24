bab6efe703de4c79fca5a7ea9182c066eef1aaaf
FERNANDA SILVA <fernanda.rodrigues.cdc@gmail.com>
2022-03-24 15:45:05 -0700

    Time: 94 ms (61.33%), Space: 42.2 MB (37.92%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -3,19 +3,21 @@
  * @param {number} high
  * @return {number}
  */
-const countOdds = (low, high) => {
-//     let count = 0;
-
-//     for (let i = low; i <= high; i++) {
-//         if (i % 2 !== 0) {
-//             count++;
-//         }
-//     }
-//     return count;
-    
+const countOdds = (low, high) => {    
     const nums = high - low;
     const n = low % 2 === 0 ? nums : nums + 1;
     
     return n / 2;
-    
 };
+
+
+const countOdds2 = (low, high) => {
+    // let count = 0;
+
+    // for (let i = low; i <= high; i++) {
+    //     if (i % 2 !== 0) {
+    //         count++;
+    //     }
+    // }
+    // return count;
+}

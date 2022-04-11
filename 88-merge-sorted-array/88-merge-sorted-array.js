73c1d89cc6801492ed7a3121b1507550ac4a4d4f
FERNANDA SILVA <fernanda.rodrigues.cdc@gmail.com>
2022-04-11 13:20:15 -0700

    Time: 56 ms (97.53%), Space: 42.3 MB (35.63%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = (nums1, m, nums2, n) => {
+    let first = m - 1;
+    let second = n -1;
+    let i = m + n - 1;
+    
+    while (second >= 0) {
+        let firstValue = nums1[first];
+        let secondValue = nums2[second];
+        
+        if (firstValue > secondValue) {
+            nums1[i] = firstValue;
+            i--;
+            first--;
+        } else {
+            nums1[i] = secondValue;
+            i--;
+            second--;
+        }
+    }
+};
+
+
+/*
+
+
+ 0  1  2  3  4  5                          0  1  2
+[1, 2, 3, 0, 0, 0]  m = 3                 [2, 5, 6]  n = 3
+
+
+first = m - 1   -> 3 - 1 = 2
+second = n - 1  -> 3 - 1 = 2
+i = m + n - 1   -> 2 + 2 - 1 = 3
+
+
+enquanto second (2 >= 0) {
+    firstValue = nums1[2]
+    secondValue = nums2[2]
+}
+
+nums1[3] = 
+
+*/

f44b156d322b6e506cb602cfcc0ad40f09ed7b03
FERNANDA SILVA <fernanda.rodrigues.cdc@gmail.com>
2022-04-13 13:32:50 -0700

    Time: 104 ms (80.78%), Space: 48.4 MB (51.45%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,27 +3,24 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = (s) => {
-    // store the running characters and a longest string length variable
-    let currentString = [];
-    let longestStringLength = 0;
-
-    // Loop through the string
+    let map = {};
+    let start = 0;
+    let maxLength = 0;
+    
     for (let i = 0; i < s.length; i++) {
-        // Attempt to get the current character's position in the current string
-        const currentCharacterPosition = currentString.indexOf(s[i]);
-
-        // Check if the current character exists in the current string
-        if (currentCharacterPosition !== -1) {
-            // Chop the array off after the occurence of the character
-            currentString.splice(0, currentCharacterPosition + 1);
+        let lastChar = s[i];
+        
+        if (map[lastChar] === undefined) {
+            map[lastChar] = 0;
         }
-
-        // Add the current character to the array
-        currentString.push(s[i]);
-
-        // Store the current string length if bigger than the existing record
-        longestStringLength = Math.max(longestStringLength, currentString.length);
+        map[lastChar] += 1;
+        
+        while (map[lastChar] > 1) {
+            let firstChar = s[start];
+            map[firstChar] -= 1;
+            start += 1;
+        }
+        maxLength = Math.max(maxLength, i - start + 1);
     }
-
-    return longestStringLength;
+    return maxLength;
 };
